b8cd3d64464b9d1f2c3d5e39afac02bc9172ed7c
Domenico Tenace <domenicotenace99@gmail.com>
2024-08-14 15:04:35 +0200

    feat: new filter date for blog list

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,2 +1,4 @@
- export const getFormattedDate = (date: Date) => date.toString().split("T")[0];
- export const getFormattedDescription = (desc: string) => desc.slice(10, 100);
+export const getFormattedDate = (date: Date) => date.toString().split("T")[0];
+export const getFormattedDescription = (desc: string) => desc.slice(10, 100);
+export const filterDate = (date: Date, monthsToFilter: number) =>
+  date >= new Date(new Date().setMonth(new Date().getMonth() - monthsToFilter));
